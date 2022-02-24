8ebfc2c4466cd71ccb70f6f9313fef0b3192915e
Kevin M. Gill <kmsmgill@gmail.com>
2022-02-24 09:01:06 -0800

    Update dustdevil.sh

--- dustdevil.sh
+++ dustdevil.sh
@@ -18,6 +18,41 @@ cd $sol/NCAM
 
 msl_fetch_raw -c NAV_RIGHT HAZ_FRONT HAZ_REAR -s $sol -S ${seqid}
 
+# NCAM Dust Devil Movie NCAM00596
+msl_fetch_raw -c NAV_RIGHT -s $sol -S NCAM00596
+
+# NCAM Cloud Shadow
+msl_fetch_raw -c NAV_RIGHT -s $sol -S NCAM00556
+
+# NCAM Zenith Movie
+msl_fetch_raw -c NAV_RIGHT -s $sol -S NCAM00551
+msl_fetch_raw -c NAV_RIGHT -s $sol -S NCAM00536
+
+# NCAM Suprahorizon Movie
+msl_fetch_raw -c NAV_RIGHT -s $sol -S NCAM00567
+
 msl_ecam_calibrate -i *JPG -v -t 2.0
 
-diffgif -i *${seqid}*-rjcal.png -o DustDevil_${sol}_${seqid}_rjcal.gif -v -b 0 -w 2.0 -g 2.5 -B 1.5 -d 20
+if [ `ls *${seqid}*-rjcal.png 2> /dev/null | wc -l` -gt 0 ]; then
+    diffgif -i *${seqid}*-rjcal.png -o DustDevil_${sol}_${seqid}_rjcal.gif -v -b 0 -w 2.0 -g 2.5 -B 1.5 -d 20
+fi
+
+if [ `ls *NCAM00556*-rjcal.png 2> /dev/null | wc -l` -gt 0 ]; then
+    diffgif -i *NCAM00556*-rjcal.png -o CloudShadow_${sol}.gif -v -b 0 -w 1.0 -g 2.5 -B 1.5 -d 20
+fi
+
+if [ `ls *NCAM00551*-rjcal.png 2> /dev/null | wc -l` -gt 0 ]; then
+    diffgif -i *NCAM00551*-rjcal.png -o ZenithMovie_${sol}.gif -v -b 0 -w 1.0 -g 2.5 -B 1.5 -d 20
+fi
+
+if [ `ls *NCAM00536*-rjcal.png 2> /dev/null | wc -l` -gt 0 ]; then
+    diffgif -i *NCAM00536*-rjcal.png -o ZenithMovie_${sol}.gif -v -b 0 -w 1.0 -g 2.5 -B 1.5 -d 20
+fi
+
+if [ `ls *NCAM00567*-rjcal.png 2> /dev/null | wc -l` -gt 0 ]; then
+    diffgif -i *NCAM00567*-rjcal.png -o SupraHorizonMovie_${sol}.gif -v -b 0 -w 1.0 -g 2.5 -B 1.5 -d 20
+fi
+
+if [ `ls *NCAM00596*-rjcal.png 2> /dev/null | wc -l` -gt 0 ]; then
+    diffgif -i *_S*NCAM00596*rjcal.png -o DustDevil596_${sol}.gif -v -b 0 -w 5.0 -g 1.5 -B 1.5 -d 40
+fi
